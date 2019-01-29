0709e643a17dd6993cd8694be58c304b57d89799
Nikhil Wagh <nikhil.g.wagh@gmail.com>
2019-01-29 23:18:00 +0530

    Local notification code uncommented.

--- TeamTTI/Utilities/Extensions.swift
+++ TeamTTI/Utilities/Extensions.swift
@@ -18,11 +18,11 @@ extension UIAlertController {
     func createSettingsAlertController(title: String, message: String) -> UIAlertController {
         let controller = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment:"" ), style: .cancel, handler: nil)
+     //   let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment:"" ), style: .cancel, handler: nil)
         let settingsAction = UIAlertAction(title: NSLocalizedString("Settings", comment:"" ), style: .default, handler: { action in
             UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!, options: [:], completionHandler: nil)
         })
-        controller.addAction(cancelAction)
+      //  controller.addAction(cancelAction)
         controller.addAction(settingsAction)
         
         return controller
