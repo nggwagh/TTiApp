0bc2405ca4907610c4137a877c51413330f90423
Mohini Mehetre <mohini.mehetre@silicus.com>
2019-02-07 20:19:29 +0530

    Update Constant.swift
    MM: ADDED CONSTANT

--- TeamTTI/Utilities/Constant.swift
+++ TeamTTI/Utilities/Constant.swift
@@ -22,6 +22,7 @@ struct Constant {
         struct User {
             static let path = "/api/v1/user/me"
             static let userID = "userId"
+            static let role = "role"
             static let currentLatitude = "currentLatitude"
             static let currentLongitude = "currentLongitude"
             static let resetPasswordAPIPath = "/api/v1/user/reset_password"
