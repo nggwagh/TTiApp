42d49255715694876b74ecd1e2fe59fc1b315ea1
Nikhil Wagh <nikhil.g.wagh@gmail.com>
2019-05-25 13:34:28 +0530

    1) StorePerformanceObjective API integrated. 2) When selecting an objective will navigate to Objective Details screen.

--- TeamTTI/Network Services/StoreApi.swift
+++ TeamTTI/Network Services/StoreApi.swift
@@ -10,10 +10,12 @@ import Foundation
 import Moya
 
 enum StoreApi {
+    
     case stores()
     case storeObjectivesFor(storeId: Int, month: String, year: Int)
     case setStoreSpentTime(regionObject: [String: Any])
     case getStorePerformanceList(regionId: Int)
+    case getStorePerformanceObjective(storeId: Int)
 
 }
 
@@ -21,12 +23,16 @@ extension StoreApi: TargetType {
     var baseURL: URL {
         
         switch self {
+            
         case .stores():
             return URL(string: "\(Constant.API.baseURL)/?showCounts=1")!
+            
         case let .storeObjectivesFor(_,month,year):
             return  URL(string: "\(Constant.API.baseURL)/?month=\(month)&year=\(year)")!
+            
         case let .getStorePerformanceList(regionId):
             return  URL(string: "\(Constant.API.baseURL)/?regionID=\(regionId)&showCounts=1")!
+            
         default:
             return Constant.API.baseURL
         }
@@ -47,19 +53,30 @@ extension StoreApi: TargetType {
         case .getStorePerformanceList(_):
             return Constant.API.Store.path + "/search"
 
+        case .getStorePerformanceObjective(let storeId):
+            return Constant.API.Store.path + "/\(storeId)/objective"
+
         }
     }
 
     var method: Moya.Method {
         switch self {
+            
         case .stores():
             return .get
+            
         case .storeObjectivesFor:
             return .get
+            
         case .setStoreSpentTime:
             return .post
+            
         case .getStorePerformanceList(_):
             return .get
+            
+        case .getStorePerformanceObjective(_):
+            return .get
+
         }
 
     }
@@ -70,16 +87,24 @@ extension StoreApi: TargetType {
 
     var task: Task {
         switch self {
+            
         case .stores():
                return .requestPlain
+            
         case .storeObjectivesFor:
             return .requestPlain
+            
         case .setStoreSpentTime(let regionObject):
             print("Geo Item parameters: \(regionObject)")
             return .requestParameters(parameters:regionObject,
                                       encoding: JSONEncoding.default)
+            
         case .getStorePerformanceList(_):
             return .requestPlain
+            
+        case .getStorePerformanceObjective(_):
+            return .requestPlain
+
         }
     }
 
--- TeamTTI/Performance/PerformanceDetailsViewController.swift
+++ TeamTTI/Performance/PerformanceDetailsViewController.swift
@@ -7,17 +7,27 @@
 //
 
 import UIKit
+import Moya
 
 class PerformanceDetailsViewController: UIViewController {
+
+    //MARK: - IBOutlets
 
     @IBOutlet weak var tableView: UITableView?
     @IBOutlet weak var storeNameLabel: UILabel?
     private var storeObjectives = [StoreObjective]()
     var storeName: String?
+    var storeId: Int?
+    private var objectiveNetworkTask: Cancellable?
+
+    //MARK: - View Lifecycle
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.storeNameLabel?.text = self.storeName
+        
+        self.getStorePerformanceObjectiveList()
+        
     }
 
     /*
@@ -30,29 +40,96 @@ class PerformanceDetailsViewController: UIViewController {
     }
     */
 
+    
+    //MARK: - Private Methods
+    
+    func getStorePerformanceObjectiveList() {
+        
+        //Show progress hud
+        self.showHUD(progressLabel: "")
+        
+        objectiveNetworkTask?.cancel()
+        
+        objectiveNetworkTask = MoyaProvider<StoreApi>(plugins: [AuthPlugin()]).request(.getStorePerformanceObjective(storeId: 2)) { result in
+            
+            // hiding progress hud
+            self.dismissHUD(isAnimated: true)
+            
+            switch result {
+                
+            case let .success(response):
+                print(response)
+                
+                if case 200..<400 = response.statusCode {
+                    
+                    do{
+                        let jsonDict = try JSONSerialization.jsonObject(with: response.data, options: []) as! [String: Any]
+                        print(jsonDict)
+                        
+                        self.storeObjectives = StoreObjective.build(from: jsonDict["objectives"] as! [[String : Any]])
+                        
+                    }
+                    catch let error {
+                        print(error.localizedDescription)
+                        Alert.show(alertType: .parsingFailed, onViewContoller: self)
+                    }
+                }
+                else
+                {
+                    print("Status code:\(response.statusCode)")
+                    Alert.show(alertType: .wrongStatusCode(response.statusCode), onViewContoller: self)
+                }
+                
+                
+                break
+            case let .failure(error):
+                print(error.localizedDescription)
+                Alert.showMessage(onViewContoller: self, title: Bundle.main.displayName, message: error.localizedDescription)
+                break
+            }
+        }
+    }
+    
+    
 }
 
+
+
+
 extension PerformanceDetailsViewController: UITableViewDataSource {
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     //  return self.storeObjectives.count
         return 5
-//        return self.storeObjectives.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "StoreObjectiveTableViewCell", for: indexPath) as? StoreObjectiveTableViewCell {
-//            cell.configure(with: self.storeObjectives[indexPath.row])
+          //  cell.configure(with: self.storeObjectives[indexPath.row])
             return cell
         }
         return UITableViewCell()
     }
+    
 }
 
 extension PerformanceDetailsViewController: UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+    let objectiveObject = self.storeObjectives[indexPath.row]
+        
+        let storyboard = UIStoryboard.init(name: "Home", bundle: nil)
+        
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "TaskDetailViewController") as! TaskDetailViewController
+        
+        destinationVC.tastDetails = objectiveObject
+        
+        self.navigationController?.pushViewController(destinationVC, animated: true)
     }
+    
 }
